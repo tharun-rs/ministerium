4349efc1899907ed527e406f635334dfea99d86f
Tharun R S <rstharun11@gmail.com>
2026-01-18 14:08:18 +0530

    log payload

--- src/handlers/github_webhooks.rs
+++ src/handlers/github_webhooks.rs
@@ -10,6 +10,20 @@ pub async fn github_webhook_handler(
     headers: HeaderMap,
     body: Bytes,
 ) -> StatusCode  {
+    if let Ok(body_str) = std::str::from_utf8(&body) {
+        println!("GitHub webhook payload:\n{}", body_str);
+    } else {
+        println!("GitHub webhook payload: <non-utf8 body>");
+    }
+
+    if let Some(event) = headers.get("X-GitHub-Event") {
+        println!("GitHub event: {:?}", event);
+    }
+
+    if let Some(delivery) = headers.get("X-GitHub-Delivery") {
+        println!("GitHub delivery ID: {:?}", delivery);
+    }
+    
     // 1. Verify signature
     if !crypto_utils::verify_signature(&headers, &body) {
         return StatusCode::UNAUTHORIZED;
