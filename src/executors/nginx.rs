dba88eec9939552ccccecb869a206467546d3189
Tharun R S <rstharun11@gmail.com>
2026-01-24 13:42:22 +0530

    always update nginx

--- src/executors/nginx.rs
+++ src/executors/nginx.rs
@@ -2,22 +2,18 @@ use crate::utils::nginx_utils::{generate_nginx_config, nginx_config_exists, relo
 use tokio::fs;
 
 pub async fn reload_nginx_if_needed(repo_name: &str, port: u16) -> Result<(), String> {
+    // 1. Write the Nginx config
+    let contents = &generate_nginx_config(repo_name, port);
+    let path = format!("/etc/nginx/conf.d/ministerium/{}.conf", repo_name);
 
-    // If nginx config doesn't exist
-    if !nginx_config_exists(repo_name) {
-        // 1. Write the Nginx config if it doesn't exist
-        let contents = &generate_nginx_config(repo_name, port);
-        let path = format!("/etc/nginx/conf.d/ministerium/{}.conf", repo_name);
+    fs::write(&path, contents)
+        .await
+        .map_err(|e| format!("failed to write nginx config: {}", e))?;
+    println!("Config file written.");
 
-        fs::write(&path, contents)
-            .await
-            .map_err(|e| format!("failed to write nginx config: {}", e))?;
-        println!("Config file written.");
-
-        // 2. Validate and reload nginx
-        reload_nginx().await?;
-        print!("Nginx Reloaded");
-    }
+    // 2. Validate and reload nginx
+    reload_nginx().await?;
+    print!("Nginx Reloaded");
 
     Ok(())
 }
